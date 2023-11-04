240a6076e907cb452a88cfe3bcb4a2e776e36660
anxi01 <tjdals2893@naver.com>
2023-11-05 00:15:29 +0900

    init

---
+++ src/main/java/com/example/subsub/SubsubApplication.java
@@ -0,0 +1,13 @@
+package com.example.subsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubsubApplication.class, args);
+    }
+
+}
