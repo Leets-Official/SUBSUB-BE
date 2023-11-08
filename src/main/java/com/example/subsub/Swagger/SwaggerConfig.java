888c07a3ce52c0ad759dd4faff74fcd39910c6c2
Vicky <vicky0523@naver.com>
2023-11-09 00:47:02 +0900

    feat:swagger구현

--- src/main/java/com/example/subsub/Swagger/SwaggerConfig.java
+++ src/main/java/com/example/subsub/Swagger/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(title = "SUBSUB API 명세서!!!! ( >.< )",
                 description = "Leets-Zero100 : SUBSUB - ToDoList 서비스 API 명세서",
                 version = "v1"))
+@RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
     @Bean
