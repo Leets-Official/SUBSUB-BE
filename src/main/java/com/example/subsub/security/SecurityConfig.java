e9562185dea044e7c56ed8aa184465442f87c3a5
taeseok yang <136783693+taeseokyang@users.noreply.github.com>
2023-11-14 23:25:40 +0900

    hotfix: cors 에러 해결을 위한 라이브러리 임포트

--- src/main/java/com/example/subsub/security/SecurityConfig.java
+++ src/main/java/com/example/subsub/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
