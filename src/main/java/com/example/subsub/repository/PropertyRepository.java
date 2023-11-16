aca0f07c201b49d2c24500fe0705710d44e7bce9
taeseok yang <136783693+taeseokyang@users.noreply.github.com>
2023-11-16 15:50:55 +0900

    hotfix: 급한순 10개에서 안한것만 가져오게 수정

--- src/main/java/com/example/subsub/repository/PropertyRepository.java
+++ src/main/java/com/example/subsub/repository/PropertyRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Integer> {
-    List<Property> findTop5ByUserOrderByExpiredAtAsc(User user);
+    List<Property> findTop5ByUserAndIsCompletedOrderByExpiredAtAsc(User user, boolean isCompleted);
 
     List<Property> findBySubjectSubjectIdOrderByIsCompletedAscExpiredAtAsc(Integer subject_subjectId);
 }
